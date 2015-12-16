ff7e927cefe88b5aa7049ed38b26fc5308695e9d
José Roberto Araújo <jroberto.araujo@gmail.com>
2015-12-16 00:58:26 -0200

    Projeto utilizado no artigo KATANA usando Host customizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace KatanaCustomSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:8090"))
+            {
+                Console.WriteLine("Servidor Katana (vNext) online !!!");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using Owin;
+using System.Diagnostics;
+
+namespace KatanaCustomSelfHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage("/");
+
+            app.UseErrorPage();
+
+            app.Run(context =>
+            {
+                Trace.WriteLine(context.Request.Uri);
+
+                var parametroVersaoQS = context.Request.Query["versao"];
+
+                try
+                {
+                    var parametroVersao = int.Parse(parametroVersaoQS);
+                }
+                catch
+                {
+                    throw new System.Exception("Erro ao converter o número da versão");
+                }
+
+                return context.Response.WriteAsync("Olá Mundo, eu sou o servidor Katana (vNext) de versão: " + parametroVersaoQS);
+            });
+        }
+    }
+}
